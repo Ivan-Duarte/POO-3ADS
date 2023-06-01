bbc761a86018f314a07cce93cbcaf7024ca782a7
Ivan-Duarte <ivangermanoduarte@gmail.com>
2023-06-01 12:13:53 -0300

    Codigo Exer 01

---
+++ Js/Exer 01 - Vingadores/src/index.js
@@ -0,0 +1,10 @@
+const personagem = {
+    nome: "Steve Rogers",
+    codinome:  "Capitão America",
+    armaPrincipal: "Escudo de Mão",
+    armaSecundaria: "",
+    velocidade: 60, // considerando 0 de 100
+    forca: 80, // considerando 0 de 100
+    resistencia: 90, // considerando 0 de 100
+    
+}
